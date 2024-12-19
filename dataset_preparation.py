79e701c4722376ebbdc7c82e64d3d7391b6c9318
zeinab-kr <zeinab_kr@aut.ac.ir>
2024-12-20 00:34:06 +0330

    dataset preparation

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -1,3 +1,17 @@
-def create_distnce_matrix(input_path: str):
-    # TODO: read data from file and create distance matrix
-    pass
+import math
+
+
+def create_distance_matrix(input_path: str):
+    with open(input_path, 'r') as file:
+        cities = [tuple(map(int, line.strip().split(','))) for line in file.readlines()]
+
+    num_cities = len(cities)
+    distance_matrix = [[0] * num_cities for _ in range(num_cities)]
+
+    for i in range(num_cities):
+        for j in range(i + 1, num_cities):
+            distance = math.sqrt((cities[i][0] - cities[j][0]) ** 2 + (cities[i][1] - cities[j][1]) ** 2)
+            distance_matrix[i][j] = distance
+            distance_matrix[j][i] = distance
+
+    return distance_matrix
