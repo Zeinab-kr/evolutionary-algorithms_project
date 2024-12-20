f2aa8968bff229351f4b807a1fd582f44ce8afab
zeinab-kr <zeinab_kr@aut.ac.ir>
2024-12-20 14:47:48 +0330

    algorithm completed

--- optimizer.py
+++ optimizer.py
@@ -1,15 +1,17 @@
 import random
 
 import crossover
+import mutation
+import termination_condition
 from individual import Individual
 from evaluation import evaluate, evaluate_all
 from selection import select_two_individual_for_crossover
 
 
 def run_algorithm(
-    distance_matrix,
-    population_size=20,
-    generation_size=30,
+        distance_matrix,
+        population_size=20,
+        generation_size=30,
 ):
     MUTATION_RATE = 0.3
     best_fitness_list = []
@@ -19,6 +21,7 @@ def run_algorithm(
 
     # create primary population
     population = primary_population_creator(population_size, genome_size)
+    evaluate_all(population, distance_matrix=distance_matrix)
 
     while True:
         # cross over
@@ -31,16 +34,20 @@ def run_algorithm(
 
         # mutation
         for individual in generated_individuals:
-            # TODO: by using a random number and 'MUTATION_RATE', decide whether to mutate the individual or not    
-            pass
+            mutation.mutation1(individual)
 
         # TODO: evaluate generated_individuals
+        evaluate_all(generated_individuals, distance_matrix=distance_matrix)
 
         # TODO: select next generation by use of 'next_generation_selection' method
+        next_generation = next_generation_selection(population, generated_individuals)
 
         # TODO: check termination condition on generated individuals
+        if termination_condition.terminate1(next_generation):
+            break
 
         # TODO: redefine 'population' for the next iteration
+        population = next_generation
 
         # don't change following codes
         best_fitness_list.append(best_fitness(population))
@@ -51,21 +58,34 @@ def run_algorithm(
 
 
 def primary_population_creator(
-    population_size: int, genome_size: int
+        population_size: int, genome_size: int
 ) -> list[Individual]:
     # TODO: this method create primary individual based on input population size and return them as list of individuals
     return [Individual(genome_length=genome_size, generate_random_genome=True) for _ in range(population_size)]
 
 
+def next_generation_selection(population: list[Individual], generated_individuals: list[Individual])\
+        -> list[Individual]:
+    combined_population = population + generated_individuals
+
+    combined_population.sort(key=lambda ind: ind.fitness, reverse=True)
+
+    next_generation_size = len(population)
+    next_generation = combined_population[:next_generation_size]
+
+    return next_generation
+
+
 def avg_fitness(
-        population:list[Individual]
+        population: list[Individual]
 ) -> float:
     # TODO: this method calculates average of individuals fitness
-    pass
+    total_fitness = sum(individual.fitness for individual in population)
+    return total_fitness / len(population) if population else 0
 
 
 def best_fitness(
-        population:list[Individual]
+        population: list[Individual]
 ) -> float:
     # TODO: this method finds best fitness in the population
-    pass
+    return max(individual.fitness for individual in population) if population else float('-inf')
