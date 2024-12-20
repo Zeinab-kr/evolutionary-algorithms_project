917c5ad02f2accd3ecaffa6c4d79cddfc281a43d
zeinab-kr <zeinab_kr@aut.ac.ir>
2024-12-20 14:20:15 +0330

    termination condition added

--- termination_condition.py
+++ termination_condition.py
@@ -1,3 +1,10 @@
 def terminate1(individual_list) -> bool:
     BEST_FITNESS = 423.741  # length of the shortest path
-    # TODO: in this method, you must check if any individual in the list has reached the 'BEST_FITNESS' value. If yes, return the individual (stop the algorithm), otherwise return None
+    # TODO: in this method, you must check if any individual in the list has reached the 'BEST_FITNESS' value. If
+    #  yes, return the individual (stop the algorithm), otherwise return None
+
+    for individual in individual_list:
+        if individual.fitness == 1 / BEST_FITNESS:
+            return True
+
+    return False
